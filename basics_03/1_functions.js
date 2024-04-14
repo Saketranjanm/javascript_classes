fd8f68bed446d389a109ad66f7256873ff357b1e
Saketranjanm <59251375+Saketranjanm@users.noreply.github.com>
2024-04-14 12:18:49 +0000

    modified

--- basics_03/1_functions.js
+++ basics_03/1_functions.js
@@ -13,3 +13,48 @@ function addTwoNumbers(num1,num2){
 }
 
 addTwoNumbers(5,9);
+
+function addTwoNumberss(num1,num2){
+    // let result = num1+num2
+    return num1+num2
+    console.log("saket");//out of scope as comes after return statement so not going to print
+ }
+ 
+ const result = addTwoNumberss(5,10);
+ console.log("Result : ", result);
+
+ function loginUserMessage(username){
+    if (username===undefined) {
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in`
+ }
+ console.log(loginUserMessage())
+ console.log(loginUserMessage("saket"))
+
+
+ function calculateCartPrice(...num1){//here ... is rest opeartor
+    return num1
+ }
+ console.log(calculateCartPrice(200,400,500));
+
+ const user = {
+    username:"saket",
+    price:199
+ }
+ function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and the price is ${anyobject.price}`);
+ }
+
+ handleObject(user)
+ handleObject({
+    username:"nikhil",
+    price:999
+ })
+
+ const myNewArray = [200,300,400,800]
+ function retrunSecondValue(getArray){
+    return getArray[1]
+ }
+ console.log(retrunSecondValue(myNewArray));
