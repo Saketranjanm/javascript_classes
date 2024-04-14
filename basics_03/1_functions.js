d2c0b3b0d930985d62a4c6b41edb13371b90adfa
Saketranjanm <59251375+Saketranjanm@users.noreply.github.com>
2024-04-14 11:52:19 +0000

    func

---
+++ basics_03/1_functions.js
@@ -0,0 +1,15 @@
+function SayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("K");
+    console.log("E");
+    console.log("T");
+
+}
+SayMyName()
+
+function addTwoNumbers(num1,num2){
+   console.log( num1+num2)
+}
+
+addTwoNumbers(5,9);
