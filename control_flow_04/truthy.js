b88f14e295e36f95947d09cfc4873a658f5c85e7
Saketranjanm <59251375+Saketranjanm@users.noreply.github.com>
2024-04-15 16:52:43 +0000

    completed

--- control_flow_04/truthy.js
+++ control_flow_04/truthy.js
@@ -21,3 +21,18 @@ if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
 
+//nullish coalescing operator(??): null undefined
+
+let val1;
+// val1 = 5 ?? 10 -> 5
+// val1 = null ?? 10 -> 10
+// val1 = undefined ?? 15 -> 15
+val1 = null ?? 10 ?? 20 //10
+
+console.log(val1);
+
+
+//terniary operator
+// condition ? true : false 
+ const ice = 100 
+ ice <= 80? console.log("less than 80") : console.log("more than 80");
